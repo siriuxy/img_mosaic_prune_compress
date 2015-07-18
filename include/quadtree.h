db879b7722a18321fc9f5e73ec0d5abafeb1582b
Likai Yan <likaiy5@gmail.com>
2015-07-18 03:53:19 +0000

    incomplete
    git-svn-id: https://subversion.ews.illinois.edu/svn/su15-cs225/lyan5/mp_qtree@5677 81a470a5-b7fd-438a-be91-513ba3ee436a

--- include/quadtree.h
+++ include/quadtree.h
@@ -21,9 +21,21 @@ class quadtree
 {
   public:
 
+	quadtree();
+	quadtree(epng::png source, unsigned d);
+	quadtree(quadtree& other);
+	quadtree(quadtree&& other);
+	~quadtree() = default;
 
+	quadtree& operator=(quadtree other);
 
+	void swap(quadtree& other);
+	void build_tree(const epng::png & source, unsigned d);
 
+	const epng::rgba_pixel& operator()(unsigned x, unsigned y) const;
+	epng::png decompress()const;
+
+	bool empty();
 
 
 
@@ -35,17 +47,28 @@ class quadtree
      */
     class node
     {
-      public:
+      public: //
         std::unique_ptr<node> northwest;
         std::unique_ptr<node> northeast;
         std::unique_ptr<node> southwest;
         std::unique_ptr<node> southeast;
 
         epng::rgba_pixel element; // the pixel stored as this node's "data"
+
+	unsigned node_dis_;//distance from this node, to root_
+	unsigned node_start_x_;
+	unsigned node_start_y_;
+
+	node(node& other);
     };
 
-    std::unique_ptr<node> root_; // the root of the tree
+//private functions:
+	copy_tree(std::unique_ptr<node> newNode, node* other);
+	std::unique_ptr<node> build_tree(unique_ptr<node> curr, unsigned side_length);
 
+//private vairaibles:
+	unsigned length_; //side length of the image.
+	std::unique_ptr<node> root_; // the root of the tree
 /**** Do not remove this line or copy its contents here! ****/
 #include "quadtree_given.h"
 };
--- src/quadtree.cpp
+++ src/quadtree.cpp
@@ -10,6 +10,79 @@
 namespace cs225
 {
 
+quadtree::quadtree(){
+	root_ = nullptr;
+	length_ = 0;
+}
 
+quadtree::quadtree(epng::png source, unsigned d){
+	build_tree(source, d);
+}
+
+quadtree::quadtree(quadtree& other){
+	//recursively copy down each unique_ptr's rgb elemnt, and build a similar tree structure
+	node* otherRoot = other.root_.get();
+	copy_tree(root_, otherRoot);
+}
+
+void quadtree::copy_tree(std::unique_ptr<node>& newNode, node* other){
+	if (!other) return;
+	else {
+		newNode = std::unique_ptr<node>(new node(*other));//how do i scope to node(copy)
+		copy_tree(newNode->northwest, other->northwest.get());
+		copy_tree(newNode->northeast, other->northeast.get());
+		copy_tree(newNode->southwest, other->southwest.get());
+		copy_tree(newNode->southeast, other->southeast.get());
+		}
+}
+quadtee::node::node(node& other){
+
+	node_dis_ = other.node_dis_;
+	node_start_x_ = other.node_start_x_;
+	node_start_y_ = other.node_start_y_;
+	element = other.element;
+
+}
+
+quadtree::quadtree(quadtree&& other):root_{nullptr},lenght_{0}{
+	swap(other);
+}
+
+quadtree& quadtree::operator=(quadtree other){
+	swap(other);
+	return *this;
+}
+
+void quadtree::swap(quadtree& other){
+	std::swap(root_, other.root_);
+	std::swap(length_, other.length_);
+}
+
+void quadtree::build_tree(const epng::png & source, unsigned d){
+	//if not empty, delete by swapping with empty tree.
+	if (!empty()) {
+		swap(quadtree());
+	}
+	build_tree(root_, d);
+}
+
+std::unique_ptr<node> quadtree::build_tree(unique_ptr<node>& curr, unsigned side_length){
+	if (length_ / (curr->node_dis_ + 1) == 1) {
+		
+		return;
+	}
+	
+
+}
+
+
+
+const epng::rgba_pixel& quadtree::operator()(unsigned x, unsigned y) const;
+
+png::png quadtree::decompress()const;
+
+bool quadtree::empty(){
+	return root_ == nullptr;
+	}
 
 }
